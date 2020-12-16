4609e03c95455b348a548efb3118c6263e57a08a
Angga <angga@linuxmail.org>
2020-12-17 00:30:51 +0700

    DCLXVI: add nanopool module

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -189,6 +189,9 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME") or None
 # Uptobox
 USR_TOKEN = os.environ.get("USR_TOKEN_UPTOBOX", None)
 
+# Nanopool
+WALLET = os.environ.get("WALLET", None)
+
 # Setting Up CloudMail.ru and MEGA.nz extractor binaries,
 # and giving them correct perms to work properly.
 if not os.path.exists("bin"):
